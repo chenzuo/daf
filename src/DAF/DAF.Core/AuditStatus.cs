3782a5442ca42d110b4f772582a43dbadbbdc7fe
cairabbit <caics.china@gmail.com>
2013-04-15 00:25:10 +0800

    add AuditStatus.cs

---
+++ src/DAF/DAF.Core/AuditStatus.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DAF.Core
+{
+    [DataContract]
+    public enum AuditStatus
+    {
+        [EnumMember]
+        Draft = 0,
+        [EnumMember]
+        Submitted = 1,
+        [EnumMember]
+        Approval = 2,
+        [EnumMember]
+        Audited = 3,
+        [EnumMember]
+        Published = 4
+    }
+}
